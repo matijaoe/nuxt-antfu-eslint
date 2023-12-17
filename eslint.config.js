b43f694243c8a2fb78e81650a4999ed7841d6585
Matija Osrečki <github@matijao.com>
2023-12-17 13:26:44 +0100

    feat: go through all unicorn rules

--- eslint.config.js
+++ eslint.config.js
@@ -34,6 +34,7 @@ export default antfu({
 
   overrides: {
     javascript: {
+      'default-param-last': 'error',
       'no-alert': 'warn',
       'no-await-in-loop': 'error',
       'no-console': ['warn', { allow: ['warn', 'error'] }],
@@ -56,11 +57,16 @@ export default antfu({
     },
 
     typescript: {
-      'default-param-last': 'error',
-      'no-throw-literal': 'error',
+      // Overwritten by ts/* rules below
+      'default-param-last': 'off',
+      'no-throw-literal': 'off',
       'no-use-before-define': 'off',
+      'prefer-destructuring': 'off',
+
       'ts/ban-ts-comment': 'off',
       'ts/consistent-type-definitions': ['error', 'type'],
+      'ts/default-param-last': 'error',
+      'ts/no-floating-promises': 'error', // check performance
       'ts/naming-convention': [
         'error',
         // TS interface
@@ -84,63 +90,30 @@ export default antfu({
         },
       ],
       'ts/no-explicit-any': 'warn',
+      'ts/no-throw-literal': 'error',
       'ts/no-use-before-define': 'off',
       'ts/prefer-as-const': 'error', // TODO: make it work in vue script
+      'ts/prefer-destructuring': 'error',
+      'ts/prefer-for-of': 'error',
       'ts/prefer-nullish-coalescing': 'error',
-      'ts/prefer-ts-expect-error': 'off',
-
-      // eslint dies
-      // 'prefer-destructuring': 'off',
-      // 'ts/prefer-destructuring': 'error',
-      // 'ts/prefer-for-of': 'error',
     },
 
     vue: {
-      // TODO
-      'vue/attributes-order': ['error', {
-        alphabetical: false,
-        order: [
-          'DEFINITION',
-          'LIST_RENDERING',
-          'CONDITIONALS',
-          'RENDER_MODIFIERS',
-          'GLOBAL',
-          ['UNIQUE', 'SLOT'],
-          'TWO_WAY_BINDING',
-          'OTHER_DIRECTIVES',
-          'OTHER_ATTR',
-          'CONTENT',
-          'EVENTS',
-        ]
-      }],
-      'vue/block-lang': ['warn', {
-        script: {
-          lang: 'ts'
-        },
-        style: {
-          lang: 'postcss'
-        },
-      }],
+      'vue/block-lang': ['warn', { script: { lang: 'ts' }, style: { lang: 'postcss' } }],
       'vue/block-order': ['error', { order: ['script', 'template', 'style'] }],
       'vue/brace-style': ['error', '1tbs', { allowSingleLine: true }],
       'vue/comma-dangle': ['error', 'only-multiline'],
       'vue/component-api-style': ['error', ['script-setup']],
       'vue/component-name-in-template-casing': ['error', 'PascalCase'],
-      'vue/define-emits-declaration': ['error', 'type-based'],
       'vue/define-emits-declaration': ['warn', 'type-literal'],
       'vue/define-props-declaration': ['error', 'type-based'],
-      'vue/first-attribute-linebreak': ['error', {
-        // this is the default
-        multiline: 'below',
-        singleline: 'ignore',
-      }],
       'vue/html-button-has-type': 'error',
-      'vue/html-self-closing': 'off',
+      'vue/html-self-closing': 'error',
       'vue/max-attributes-per-line': ['error', {
         multiline: { max: 1 },
         singleline: { max: 20 },
       }],
-      'vue/no-bare-strings-in-template': 'off', // to be used for localization
+      // 'vue/no-bare-strings-in-template': 'error', // to be used for localization
       'vue/no-duplicate-attr-inheritance': 'error',
       'vue/no-empty-component-block': 'warn',
       'vue/no-multiple-objects-in-class': 'error',
@@ -149,7 +122,7 @@ export default antfu({
       'vue/no-restricted-custom-event': ['warn', {
         event: 'input',
         message: 'If you intend a prop for v-model, it should be \'update:modelValue\' in Vue 3.',
-        suggest: 'update:modelValue'
+        suggest: 'update:modelValue',
       }, {
         event: '/ed$/',
         message: 'Event should be in imperative mood, e.g. \'edit\' instead of \'edited\'.',
@@ -158,18 +131,18 @@ export default antfu({
         'error',
         {
           element: 'a',
-          message: 'Use <NuxtLink> over <a> for Nuxt applications, or <RouterLink> for Vue apps'
+          message: 'Use <NuxtLink> over <a> for Nuxt applications, or <RouterLink> for Vue apps',
         },
         {
           element: 'marquee',
-          message: 'Do not use deprecated HTML tags'
-        }
+          message: 'Do not use deprecated HTML tags',
+        },
       ],
       'vue/no-root-v-if': 'error',
-      'vue/no-setup-props-reactivity-loss': 'error', // set to 'off' if using experimental propsDestructure
+      'vue/no-setup-props-reactivity-loss': 'error', // set to 'off' when using experimental propsDestructure
       'vue/no-side-effects-in-computed-properties': 'error', // ! does not work
       'vue/no-static-inline-styles': 'warn',
-      'vue/no-undef-properties': 'error', // useful when using js, not needed in ts
+      // 'vue/no-undef-properties': 'error', // useful in js, not needed in ts
       'vue/no-useless-mustaches': 'error',
       'vue/no-useless-template-attributes': 'error',
       'vue/no-v-text': 'error',
@@ -186,13 +159,106 @@ export default antfu({
     'antfu/top-level-function': 'off',
 
     'curly': ['error', 'all'],
+    // 'max-statements-per-line': 'off', // deprecated, but only it seems to show errors (without running the eslint command)
+    'no-negated-condition': 'off',
 
+    // https://eslint.style/
     'style/array-bracket-newline': ['error', 'consistent'],
     'style/arrow-parens': ['error', 'always'],
     'style/brace-style': ['error', '1tbs', { allowSingleLine: true }],
     'style/comma-dangle': ['error', 'only-multiline'],
     'style/eol-last': ['error', 'always'],
     'style/max-statements-per-line': ['error', { max: 2 }],
+
+    // https://github.com/sindresorhus/eslint-plugin-unicorn
+    'unicorn/better-regex': 'error',
+    'unicorn/catch-error-name': ['error', { name: 'error' }],
+    'unicorn/consistent-destructuring': 'error',
+    // 'unicorn/consistent-function-scoping': 'error', // don't know if i like this
+    'unicorn/empty-brace-spaces': 'error',
+    // 'unicorn/explicit-length-check': 'error', // I dont like this
+    'unicorn/filename-case': [
+      'error',
+      // would be nice if we could enforce PascalCase on vue components as well
+      {
+        case: 'kebabCase',
+        ignore: [
+          '.vue$',
+          '[README].md$',
+        ],
+      },
+    ],
+    // 'unicorn/no-array-callback-reference': 'error', // 😑
+    'unicorn/new-for-builtins': 'error',
+    // 'unicorn/no-array-for-each': 'error', // 😑
+    'unicorn/no-array-method-this-argument': 'error',
+    'unicorn/no-array-push-push': 'error',
+    'unicorn/no-await-expression-member': 'error',
+    'unicorn/no-console-spaces': 'error',
+    'unicorn/no-empty-file': 'error',
+    'unicorn/no-for-loop': 'error',
+    'unicorn/no-invalid-remove-event-listener': 'error',
+    'unicorn/no-keyword-prefix': 'off',
+    'unicorn/no-lonely-if': 'error',
+    'unicorn/no-negated-condition': 'error', // * really make sure we want this
+    'unicorn/no-null': 'error', // ! controversial
+    'unicorn/no-object-as-default-parameter': 'error',
+    'unicorn/no-typeof-undefined': 'error',
+    'unicorn/no-unnecessary-await': 'error',
+    'unicorn/no-unreadable-array-destructuring': 'error',
+    'unicorn/no-unreadable-iife': 'error',
+    'unicorn/no-useless-length-check': 'error',
+    'unicorn/no-useless-promise-resolve-reject': 'error',
+    'unicorn/no-useless-spread': 'error',
+    'unicorn/no-useless-switch-case': 'error',
+    // 'unicorn/no-useless-undefined': 'error',
+    'unicorn/no-zero-fractions': 'error',
+    'unicorn/number-literal-case': 'error',
+    'unicorn/numeric-separators-style': 'error',
+    'unicorn/prefer-add-event-listener': 'error',
+    'unicorn/prefer-array-find': 'error',
+    'unicorn/prefer-array-flat': 'error',
+    'unicorn/prefer-array-flat-map': 'error',
+    'unicorn/prefer-array-index-of': 'error',
+    'unicorn/prefer-array-some': 'error',
+    'unicorn/prefer-at': ['error', { checkAllIndexAccess: true }],
+    'unicorn/prefer-blob-reading-methods': 'error',
+    'unicorn/prefer-code-point': 'error',
+    'unicorn/prefer-date-now': 'error',
+    'unicorn/prefer-default-parameters': 'error',
+    'unicorn/prefer-dom-node-append': 'error',
+    'unicorn/prefer-dom-node-dataset': 'error',
+    'unicorn/prefer-dom-node-remove': 'error',
+    'unicorn/prefer-dom-node-text-content': 'error',
+    'unicorn/prefer-export-from': 'error',
+    'unicorn/prefer-keyboard-event-key': 'error',
+    'unicorn/prefer-logical-operator-over-ternary': 'error',
+    'unicorn/prefer-math-trunc': 'error',
+    'unicorn/prefer-modern-dom-apis': 'error',
+    'unicorn/prefer-modern-math-apis': 'error',
+    'unicorn/prefer-module': 'error',
+    'unicorn/prefer-native-coercion-functions': 'error',
+    'unicorn/prefer-negative-index': 'error',
+    'unicorn/prefer-object-from-entries': 'error',
+    'unicorn/prefer-optional-catch-binding': 'error',
+    'unicorn/prefer-prototype-methods': 'off',
+    'unicorn/prefer-query-selector': 'error',
+    // 'unicorn/prefer-reflect-apply': 'error',
+    'unicorn/prefer-regexp-test': 'error',
+    'unicorn/prefer-set-has': 'error',
+    'unicorn/prefer-set-size': 'error',
+    'unicorn/prefer-spread': 'error',
+    'unicorn/prefer-string-replace-all': 'error',
+    'unicorn/prefer-string-slice': 'error',
+    'unicorn/prefer-string-trim-start-end': 'error',
+    // 'unicorn/prefer-switch': 'error',
+    'unicorn/prefer-ternary': 'error',
+    'unicorn/prefer-top-level-await': 'error',
+    'unicorn/relative-url-style': 'error',
+    'unicorn/require-number-to-fixed-digits-argument': 'error',
+    'unicorn/switch-case-braces': 'error',
+    'unicorn/template-indent': 'error',
+    'unicorn/text-encoding-identifier-case': 'error',
   },
 
 })
